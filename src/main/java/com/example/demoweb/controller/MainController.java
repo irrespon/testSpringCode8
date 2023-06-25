592ab022be0abb187094f3c84e89eaf29b5319b7
AKorneev <korneevkot@yandex.ru>
2023-06-25 21:17:01 +0300

    springWeb, add attribute to controller

--- src/main/java/com/example/demoweb/controller/MainController.java
+++ src/main/java/com/example/demoweb/controller/MainController.java
@@ -1,12 +1,15 @@
 package com.example.demoweb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
     @RequestMapping("/start")
-    public String start(){
+    public String start(Model page){
+        page.addAttribute("username","Alex");
+        page.addAttribute("color","red");
         return "start.html";
     }
 }
