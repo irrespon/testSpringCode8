f1119b85101e5419c9044d9b29f95217521722f3
AKorneev <korneevkot@yandex.ru>
2023-06-26 23:07:52 +0300

    add method @Post to controller add input to html

--- src/main/java/com/example/demoweb/controller/ProductsController.java
+++ src/main/java/com/example/demoweb/controller/ProductsController.java
@@ -1,9 +1,12 @@
 package com.example.demoweb.controller;
 
+import com.example.demoweb.model.Product;
 import com.example.demoweb.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProductsController {
@@ -17,6 +20,16 @@ public class ProductsController {
     @GetMapping("/products")
     public String viewProducts(Model model){
         model.addAttribute("products",productService.findAll());
+        return "products.html";
+    }
+
+    @PostMapping("/products")
+    public String addProduct(@RequestParam String name, double price, Model model){
+        Product product = new Product();
+        product.setName(name);
+        product.setPrice(price);
+        productService.addProduct(product);
+        model.addAttribute("products", productService.findAll());
         return "products.html";
     }
 }
